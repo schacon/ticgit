0638c57fb15c9b65a523a82ff2343de931b35c81
Kevin Ballard <kevin@sb.org>
2008-12-28 20:12:35 -0500

    Print an elipsis at the end of truncated fields
    This is almost certainly going to break on non-UTF8 consoles.

--- lib/ticgit/cli.rb
+++ lib/ticgit/cli.rb
@@ -383,7 +383,7 @@ module TicGit
     def just(value, size, side = 'l')
       value = value.to_s
       if value.size > size
-        value = value[0, size]
+        value = value[0, size-1] + '…'
       end
       if side == 'r'
         return value.rjust(size)
