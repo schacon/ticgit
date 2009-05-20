6a9739584f182ce0561a58a5c0caa61efd183bc4
Michael Fellinger <m.fellinger@gmail.com>
2009-05-20 19:37:20 +0900

    Remove the cucumber specs

--- features/support/cli.rb
+++
@@ -1,7 +0,0 @@
-module CliHelpers
-  def ti_cmd
-    @ti_cmd ||= File.expand_path(File.dirname(__FILE__) + "/../../../../bin/ti")
-  end
-end
-
-World { |world| world.extend CliHelpers }
--- features/support/env.rb
+++
@@ -1,2 +0,0 @@
-require 'cucumber'
-require 'spec'
