159f9b343e84a1ac4eee716194b2c3ae3769cb7f
Jeff Welling <Jeff.Welling@Gmail.com>
2009-10-09 02:57:41 -0700

    Fixed 'ti assign' and associated options
    Was broken, suspected commit 06e1ed79d9e5ba14697d03ecd210574db3b7ed08
    by manveru.  Now works happily.

--- lib/ticgit/command/assign.rb
+++ lib/ticgit/command/assign.rb
@@ -7,6 +7,7 @@ module TicGit
     # ti assign {1}         (assign ticket to current user)
     # ti assign -c {1}      (assign ticket to current user and checkout the ticket)
     # ti assign -u {name}   (assign ticket to specified user)
+    # ti assign -u {name} {1} (assign specified ticket to specified user)
     module Assign
       def parser(opts)
         opts.banner = "Usage: ti assign [options] [ticket_id]"
@@ -19,12 +20,23 @@ module TicGit
           options.checkout = v
         }
       end
-
+      def execute
+        handle_ticket_assign
+      end
       def handle_ticket_assign
         tic.ticket_checkout(options.checkout) if options.checkout
-
-        tic_id = args[0]
-        tic.ticket_assign(options[:user], tic_id)
+        if ARGV.length == 1  #ti assign
+          tic_id=nil
+        elsif ARGV.length == 2 #ti assign {ticid}
+          tic_id=ARGV[2]
+        elsif ARGV.length == 3 #ti assign -u/-c {user/ticid}
+          if options.user
+            tic_id=nil
+          else
+            tic_id=options.checkout
+          end
+        end
+        tic.ticket_assign((options.user rescue nil), tic_id)
       end
     end
   end
