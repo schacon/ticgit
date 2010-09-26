9deab2d37997ef3c1e02d437ac7a0f4695ae2360
Jeff Welling <jeff.welling@gmail.com>
2010-09-25 20:14:39 -0700

    Added note about seemingly superflous usage
    - 'ti recent' does not take any arguments, and yet ticket_recent() is
      passed args[0]. Added a note in the source about this, will likely
      delete note and change source code in the future.

--- lib/ticgit/command/recent.rb
+++ lib/ticgit/command/recent.rb
@@ -6,6 +6,9 @@ module TicGit
       end
 
       def execute
+        # "args[0]" seems to be superfluous.  It's usage
+        # is undocumented, and supplying an argument
+        # doesn't seem to do anything.
         tic.ticket_recent(args[0]).each do |commit|
           sha = commit.sha[0, 7]
           date = commit.date.strftime("%m/%d %H:%M")
