e2d65870dcc4aea64dedba9714591efc080cec27
Jeff Welling <jeff.welling@gmail.com>
2010-10-19 22:12:48 -0700

    Added a spec for merging ticgit branches
    - Added spec file for merging ticgit branches
    - Populated with one spec for merging fast-forwards
    - Still need to clean up the spec, and add more specs for merge
      conflicts

---
+++ spec/merging_spec.rb
@@ -0,0 +1,50 @@
+require File.dirname(__FILE__) + "/spec_helper"
+
+describe TicGit do
+  include TicGitSpecHelper
+
+  before(:all) do
+    @path= setup_new_git_repo
+    @orig_test_opts= test_opts
+    @ticgit= TicGit.open(@path, @orig_test_opts)
+  end
+
+  after(:all) do
+    Dir.glob(File.expand_path("~/.ticgit/-tmp*")).each {|file_name| FileUtils.rm_r(file_name, {:force=>true,:secure=>true}) }
+    Dir.glob(File.expand_path("/tmp/ticgit-*")).each {|file_name| FileUtils.rm_r(file_name, {:force=>true,:secure=>true}) }
+  end
+
+  it "Should merge in tickets from a remote source" do
+    #FIXME Clean this up some
+    tmp_dir=Dir.mktmpdir 'ticgit-gitdir2-'
+    Dir.chdir(File.expand_path(tmp_dir)) do
+      #prep, get temp dirs, init git2
+      @ticgit.ticket_new('my new ticket')
+      git2=Git.clone(@path, 'remote_1')
+      git=Git.open(@path)
+      git_path_2= tmp_dir + '/remote_1/'
+
+      #Make ticgit branch in remote_1
+      git2.checkout('origin/ticgit')
+      git2.branch('ticgit').checkout
+      ticgit2=TicGit.open(git_path_2, @orig_test_opts)
+
+      ticgit2.ticket_new('my second ticket')
+      git2.checkout('master')
+
+      git.add_remote('upstream', git_path_2)
+      git.checkout('ticgit')
+      git.pull('upstream', 'upstream/ticgit')
+      git.checkout('master')
+
+      #Without calling reset_ticgit, the following line only shows 1 ticket.
+      @ticgit.reset_ticgit
+      #@ticgit.tickets.length
+      
+      ticgit2.tickets.length.should == @ticgit.tickets.length
+    end
+  end
+
+  it "Should not enounter merge conflicts"
+
+end
