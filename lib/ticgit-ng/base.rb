81b5c211ec77c0c5bcdd5ef9a98b3c7df9c9931e
Jeff Welling <jeff.welling@gmail.com>
2011-02-21 20:22:14 -0500

    Started adding Base.which_branch?()
    - Started which_branch?() to return the name of the ticgit branch to
      use. Should return the name of the branch as a string.  Should return
    'ticgit' if ~/.ticgit and 'ticgit' branch exist, otherwise 'ticgit-ng'

--- lib/ticgit-ng/base.rb
+++ lib/ticgit-ng/base.rb
@@ -360,6 +360,10 @@ module TicGitNG
     def new_file(name, contents)
       File.open(name, 'w+'){|f| f.puts(contents) }
     end
+    def self.which_branch?
+      #If has ~/.ticgit dir, and 'ticgit' branch
+      #If has ~/.ticgit-ng dir, and 'ticgit-ng' branch, and not ~/.ticgit dir and not 'ticgit' branch
+    end
 
   end
 end
