470d7afa8de9aa612cf711a7c07d702bdbcc3a1d
Jeff Welling <jeff.welling@gmail.com>
2011-04-05 06:00:26 -0400

    Fixed bug where 'ti comment' was prepopulated with a ruby object

--- lib/ticgit-ng/command/comment.rb
+++ lib/ticgit-ng/command/comment.rb
@@ -27,7 +27,7 @@ module TicGitNG
         elsif file
           tic.ticket_comment(File.read(file), tid)
         else
-          return unless message = get_editor_message(tic)
+          return unless message = get_editor_message
           tic.ticket_comment(message.join(''), tid)
         end
       end
